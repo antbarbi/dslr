acd89c558e03ac4160d9a48fd609c9a3e044db5d
abarbier <ant.barbier41@gmail.com>
2024-09-20 20:15:57 +0200

    first implem

---
+++ logreg_model.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+class LogRegModel:
+    def __init__(self, num_features):
+        self.num_features = num_features
+        self.weights = np.zeros(num_features)
+        self.bias = 0.0
+
+
+    def predict(self, x: pd.DataFrame) -> pd.DataFrame:
+        return 1.0 / (1.0 + np.exp(-(np.dot(x, self.weights) + self.bias)))
+
+
+    def fit(self, x: pd.DataFrame, y: pd.DataFrame, lr: float, epochs: int):
+        for epoch in tqdm(range(epochs)):
+            loss = self.update(x, y, lr)
+        return loss
+
+
+    def update(self, x: pd.DataFrame, y: pd.DataFrame, lr: float):
+        m = x.shape[0]
+        y_pred = self.predict(x)
+
+        y_pred = np.clip(y_pred, 1e-15, 1 - 1e-15)
+
+        self.weights -= lr * (1 / m) * np.dot(x.T, y_pred - y)
+        self.bias -= lr * (1 / m) * np.sum(y_pred - y)
+
+        return (-1/m) * np.sum(y * np.log(y_pred) + (1 - y) * np.log(1 - y_pred))
+
+
+    def score(self, x: pd.DataFrame, y: pd.DataFrame) -> float:
+        y_pred = self.predict(x)
+        accuracy = np.mean(y_pred == y)
+        # print(f"Weights: {self.weights}, Bias: {self.bias}")
+        return accuracy
--- logreg_train.py
+++ logreg_train.py
@@ -2,8 +2,14 @@ import argparse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from logreg_model import LogRegModel
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.tools.tools import add_constant
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
 
 
 def parse() -> argparse.Namespace:
@@ -19,6 +25,55 @@ def parse() -> argparse.Namespace:
 
 def main():
     data = pd.read_csv(parse().dataset)
+    
+    # Preprocess data
+    data.drop([
+                "Index", 
+                "First Name", 
+                "Last Name", 
+                "Birthday", 
+                "Best Hand",
+                "Charms",
+                "Ancient Runes",
+                "Defense Against the Dark Arts",
+                "Astronomy",
+                "Flying",
+            ],
+            axis=1,
+            inplace=True)
+
+    imputer = SimpleImputer(strategy='mean')
+
+    X = data.drop("Hogwarts House", axis=1)
+    X = pd.DataFrame(imputer.fit_transform(X), columns=X.columns)
+    y = data["Hogwarts House"]
+
+    le = LabelEncoder()
+    y = le.fit_transform(data["Hogwarts House"])
+
+    scaler = MinMaxScaler()
+    X = pd.DataFrame(scaler.fit_transform(X))
+
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Train model
+    model = LogRegModel(X_train.shape[1])
+    for lr in np.linspace(0.001, 0.5, 100):  # Explore learning rates from 0.001 to 1
+            loss = model.fit(X_train, y_train, lr, 200)
+            print(loss)
+            accuracy = model.score(X_test, y_test)
+            print(f"Accuracy: {accuracy * 100:.2f}%")
+
+
+    ##### Test with sklearn's LogisticRegression
+    from sklearn.linear_model import LogisticRegression
+
+    model = LogisticRegression(multi_class='multinomial', max_iter=4000)
+    model.fit(X_train, y_train)
+    accuracy = model.score(X_test, y_test)
+    print(f"Accuracy: {accuracy * 100:.2f}%")
+
 
 
 if __name__ == "__main__":
