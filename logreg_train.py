c06e7a11d4445654122f1ca4de0bd2605ea3c911
abarbier <ant.barbier41@gmail.com>
2024-09-21 02:52:14 +0200

    prepare for main merge

--- logreg_train.py
+++ logreg_train.py
@@ -55,7 +55,7 @@ def main():
     X = pd.DataFrame(scaler.fit_transform(X))
 
     # Split data
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     # Train model
 
